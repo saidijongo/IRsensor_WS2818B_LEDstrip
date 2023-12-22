e5beb15cc16d865d9ecb2192e2e1387d3cec9848
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-12-22 16:44:42 +0900

    Create mixedRG.ino

---
+++ mixedRG.ino
@@ -0,0 +1,44 @@
+#include <FastLED.h>
+
+#define LED_PIN 11
+#define IR_PIN 12
+#define NUM_LEDS 32
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  Serial.begin(900);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  pinMode(IR_PIN, INPUT);
+}
+
+void blinkLEDs() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (i % 2 == 0) {
+      leds[i] = CRGB::Green;
+    } else {
+      leds[i] = CRGB::Red;
+    }
+  }
+  FastLED.show();
+  delay(500);
+
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  FastLED.show();
+  delay(500);
+}
+
+void loop() {  
+  // Check if IR sensor is interrupted
+  if (digitalRead(IR_PIN) == LOW) {
+    unsigned long startTime = millis();
+    while (millis() - startTime < 1000) { 
+      blinkLEDs();
+      delay(50);
+    }
+
+    fill_solid(leds, NUM_LEDS, CRGB::Black);  // Turn off LEDs
+    FastLED.show();
+  }
+  delay(100);
+}
